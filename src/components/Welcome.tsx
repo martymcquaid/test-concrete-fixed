78697d7befe6fc06bea49818e646f2b1799ceaa2
martymcquaid <martymcquaid@users.noreply.github.com>
2025-11-30 19:33:16 +0000

    Add src/components/Welcome.tsx

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome: React.FC = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gradient-to-r from-blue-400 to-purple-600 text-white text-4xl'>
+      Hello World
+    </div>
+  );
+};
+
+export default Welcome;
